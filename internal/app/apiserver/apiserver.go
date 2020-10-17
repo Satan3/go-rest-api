c6b242a3879602dc429db466e8af8e199b6530ea
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-10-17 16:51:20 +0300

    Adding configuration methods for logging and routing with test

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,15 +1,55 @@
 package apiserver
 
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+)
+
 // APIServer ...
 type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
 }
 
 // New ...
-func New() *APIServer {
-	return &APIServer{}
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
 }
 
 // Start ...
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+	s.configureRouter()
+	s.logger.Info("Starting API server")
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		io.WriteString(writer, "Hello")
+	}
 }
