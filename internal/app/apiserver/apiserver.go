8eaaf0693f39ba0bf1d1f0c5b51cf60ecab444b5
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-10-17 15:27:31 +0300

    Adding APIServer folder

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,15 @@
+package apiserver
+
+// APIServer ...
+type APIServer struct {
+}
+
+// New ...
+func New() *APIServer {
+	return &APIServer{}
+}
+
+// Start ...
+func (s *APIServer) Start() error {
+	return nil
+}
